3cf608c0730248b5ca5f40381275f736e6ec3370
JeffreyOh <ohtaehyun7@naver.com>
2022-04-19 22:40:35 +0900

    WebSecurityConfiguration 생성

---
+++ src/main/java/com/threedotthree/infrastructure/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.threedotthree.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제
+            .csrf().disable() // csrf 보안 토큰 disable
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 인증으로 세션 사용하지 않음
+            .and().cors()
+            .and().authorizeRequests() // 요청에 대한 사용권한 체크
+            .anyRequest().permitAll();
+    }
+}
